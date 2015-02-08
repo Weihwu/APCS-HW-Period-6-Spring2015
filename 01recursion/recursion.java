3754257f39c02bd4b1a031318f019864b0f154ff
Wei Wu <weihwu@yahoo.com>
2015-02-08 00:43:16 -0500

    added fibonacci

--- 01recursion/recursion.java
+++ 01recursion/recursion.java
@@ -13,4 +13,17 @@ public class recursion{
 	    return fact(n-1) * n;
 	}
     }
+    
+    public int fib(int n){
+	return fibhelp(0,1,n);
+    }
+    public int fibhelp(int a, int b, int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}else if (n==0){
+	    return a;
+	}else{
+	    return fibhelp(b, a+b, n-1);
+	}
+    }
 }
