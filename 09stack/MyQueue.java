ff871f763dc4d96f1a4217b5ceea5f582a9838d4
Wei Wu <weihwu@yahoo.com>
2015-03-24 01:06:38 -0400

    testsed queue

--- 09stack/MyQueue.java
+++ 09stack/MyQueue.java
@@ -13,10 +13,20 @@ public class MyQueue<T>{
 	return true;
     }
 
-    public T dequeue(T e){
+    public T dequeue(){
 	return list.remove();
     }
 
     public static void main(String[] args){
+	MyQueue<String> a = new MyQueue<String>();
+	
+	out(a.enqueue("Hi"));
+	out(a.enqueue("Bye"));
+
+	out(a.dequeue());
+    }
+
+    public static void out(Object phrase){
+	System.out.println(phrase);
     }
 }
