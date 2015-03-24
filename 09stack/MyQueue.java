c2a268637942bf771f1b62e464386e5310466669
Wei Wu <weihwu@yahoo.com>
2015-03-24 01:02:38 -0400

    made functions for queue

--- 09stack/MyQueue.java
+++ 09stack/MyQueue.java
@@ -4,12 +4,19 @@ public class MyQueue<T>{
 
     private MyLinkedList<T> list;
 
+    public MyQueue(){
+	list = new MyLinkedList<T>();
+    }
+
     public boolean enqueue(T e){
+	list.add(e);
 	return true;
     }
 
     public T dequeue(T e){
-	return e;
+	return list.remove();
     }
 
+    public static void main(String[] args){
+    }
 }
