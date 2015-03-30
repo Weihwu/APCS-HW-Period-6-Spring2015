926d51ea9606ab378e22fe34fb38cbd0d7ac5ab3
Wei Wu <weihwu@yahoo.com>
2015-03-30 00:07:10 -0400

    added get and remove First and Last

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MyDeque<T>{
 
     Object[] ary;
@@ -5,7 +7,7 @@ public class MyDeque<T>{
     int tail;
 
     public MyDeque(){
-	ary = new Object[100];
+	ary = new Object[10];
 	head = ary.length/2;
 	tail = head;
     }
@@ -27,14 +29,74 @@ public class MyDeque<T>{
 	    ary[++tail] = value;
 	}
     }
-    
+
+    public T removeFirst(){
+	if (ary[head] == null){
+	    throw new NoSuchElementException();
+	}
+	T holder = (T)ary[head];
+	ary[head] = null;
+	if(head >= ary.length-1){
+	    head = 0;
+	}else{
+	    head++;
+	}
+	return holder;
+    }
+
+    public T removeLast(){
+	if (ary[tail] == null){
+	    throw new NoSuchElementException();
+	}
+	T holder = (T)ary[tail];
+	ary[tail] = null;
+	if (tail == 0){
+	    tail = ary.length-1;
+	}else{
+	    tail--;
+	}
+	return holder;
+    }
+
+    public T getFirst(){
+	if (ary[head] == null){
+	    throw new NoSuchElementException();
+	}
+	return (T)ary[head];
+    }
+
+    public T getLast(){
+	if (ary[tail] == null){
+	    throw new NoSuchElementException();
+	}
+	return (T)ary[tail];
+    }
+
+    public String toString(){
+	return Arrays.toString(ary);
+    }
+
     public static void main(String[] args){
 	
 	MyDeque<String> a = new MyDeque<String>();
 
 	a.addFirst("hi");
+	out(a);
 	a.addFirst("hello");
+	out(a);
 	a.addLast("bye");
+	out(a);
 	a.addLast("srly");
+	out(a);
+	a.removeFirst();
+	out(a);
+	a.removeLast();
+	out(a);
+	out(a.getFirst());
+	out(a.getLast());
+    }
+
+    public static void out(Object phrase){
+	System.out.println(phrase);
     }
 }
