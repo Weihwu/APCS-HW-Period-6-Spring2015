6e35ffe674518abe2f1d31af736cee5bdfb08963
Weihwu <weihwu@yahoo.com>
2015-03-31 12:31:03 -0400

    started maze

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -1,6 +1,42 @@
-public class Maze{
-    
-    public Maze{
-    }
+import java.util.*;
+import java.io.*;
 
+public class Maze{
+
+    private char[][] grid;
+    private int maxx, maxy;
+    private int startx, starty;
+    
+    public Maze(String filename){
+	startx = -1;
+	starty = -1;
+	String ans ="";
+	try{
+
+	    Scanner in = new Scanner(new File(filename));
+	    while (in.hasNext()){
+		String line = in.nextLine();
+		if (maxy == 0){
+		    maxx = line.length();
+		}
+		maxy++;
+		ans += line;
+	    }
+
+	}catch (Exception e){
+	    System.out.println("File: " + filename + " could not be opened.");
+	    e.printStackTrace();
+	    System.exit(0);
+	}
+
+	grid = new char[maxx][maxy];
+	for (int i = 0; i < ans.length(); i++){
+	    char c = ans.charAt(i);
+	    grid[i%maxx][i/maxx] = c;
+	    if (c == '5'){
+		startx = i%maxx;
+		starty = i/maxx;
+	    }
+	}
+    }
 }
